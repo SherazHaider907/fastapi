6bc7cb3123d1e99119529f7b639f0f75036398d4
SherazHaider907 <Sherazhaider665@gmail.com>
2026-04-06 17:55:18 +0500

    user swagger ui in fastapi

--- book.py
+++ book.py
@@ -2,6 +2,44 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/api-endpoint")
-async def book_api():
-    return {"message": "Welcome to the Book API!"}
+
+BOOKS = [
+    {
+        'title': 'The Great Gatsby',
+        'author': 'F. Scott Fitzgerald',
+        'category': 'Classic Literature',
+        'published_year': 1925
+    },
+
+    {
+        'title': 'To Kill a Mockingbird',
+        'author': 'Harper Lee',
+        'category': 'Fiction',
+        'published_year': 1960
+    },
+
+    {
+        'title': '1984',
+        'author': 'George Orwell',
+        'category': 'Dystopian Fiction',
+        'published_year': 1948
+    },
+
+    {
+        'title': 'Pride and Prejudice',
+        'author': 'Jane Austen',
+        'category': 'Romance',
+        'published_year': 1813
+    },
+
+    {
+        'title': 'The Catcher in the Rye',
+        'author': 'J.D. Salinger',
+        'category': 'Fiction',
+        'published_year': 1951
+    }
+]
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
