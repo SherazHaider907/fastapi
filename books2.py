3120d346c8dea2a348bc20198e6b167ac50a83e1
SherazHaider907 <Sherazhaider665@gmail.com>
2026-04-08 15:40:30 +0500

    model_config

--- books2.py
+++ books2.py
@@ -21,11 +21,22 @@ class Book:
         self.rating = rating
 
 class BookRequest(BaseModel):
-    id: Optional[int] = None
+    id: Optional[int] = Field(description = 'ID is not needed to create a book',default=None)
     title: str = Field(min_length=3)
     auther: str = Field(min_length=1)
     description: str = Field(min_length=1 , max_length=100)
     rating: int = Field(gt=-1, lt=6)
+
+    model_config = {
+        "json_schema_extra":{
+            "example":{
+                "title": "A new book",
+                "auther": "Harvard University",
+                "description": "A new description for the a book",
+                "rating": 5
+            }
+        }
+    }
 
 BOOKS = [
     Book(
