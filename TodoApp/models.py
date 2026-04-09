c4c7c5dc02611ce388bcf125b9b65f4545749e31
SherazHaider907 <Sherazhaider665@gmail.com>
2026-04-09 11:18:41 +0500

    add models and table in database

---
+++ TodoApp/models.py
@@ -0,0 +1,16 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean
+
+class Todo(Base):
+    __tablename__ = "todos"
+
+    id  = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priorty = Column(Integer)
+    complete = Column(Boolean, default=False)
+
+
+    
+
+
