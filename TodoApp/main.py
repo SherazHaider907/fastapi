4f8d25d949a7dc7d48f0f2120e9ff10185ea4526
SherazHaider907 <Sherazhaider665@gmail.com>
2026-04-09 13:16:06 +0500

    get data api from the models

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends
 import models
-from database import engine
+from models import Todo
+from database import engine,SessionLocal
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+bd_dependency = Annotated[Session, Depends(get_db)]
+
+@app.post("/")
+async def read_all(db: bd_dependency):
+    return db.query(Todo).all()
