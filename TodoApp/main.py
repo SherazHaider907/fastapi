048838bb869e280bee92cbcee192d694cda6238c
SherazHaider907 <Sherazhaider665@gmail.com>
2026-04-09 15:40:20 +0500

    delete request

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -73,3 +73,14 @@ async def update_todo(
     db.add(todo_model)
     db.commit()
 
+
+# delete todo by id using delete method
+@app.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todo).filter(Todo.id == todo_id).first()
+
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    db.query(Todo).filter(Todo.id == todo_id).delete()
+    
+    db.commit()
