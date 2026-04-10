3ef0b2306850e7ee78ac4537fd4dfe4659e3618e
SherazHaider907 <Sherazhaider665@gmail.com>
2026-04-10 17:47:07 +0500

    add new user table in the database and create auth user

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,10 +1,29 @@
 from fastapi import APIRouter
-
+from pydantic import BaseModel
+from models import User
 router = APIRouter()
 
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role : str
 
-@router.get("/auth/")
-async def auth():
-    return {"user": "Authenticated User"}
+@router.post("/auth/")
+async def create_user(CreateUserRequest: CreateUserRequest):
+    create_user_model = User(
+        email=CreateUserRequest.email,
+        username=CreateUserRequest.username,
+        first_name=CreateUserRequest.first_name,
+        last_name=CreateUserRequest.last_name,
+        role=CreateUserRequest.role,
+        hashed_password=CreateUserRequest.password,
+        is_active=True
+    )
+
+    return create_user_model
+    
 
 
