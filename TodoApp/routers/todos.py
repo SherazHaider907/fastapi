b6bb15b2dfe35beda2d61b4850a6df0ca1001076
SherazHaider907 <Sherazhaider665@gmail.com>
2026-04-11 16:38:31 +0500

    get authenticated by user and get the todos

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel,Field
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends, Path ,Query ,HTTPException
 from starlette import status
+from .auth import get_current_user
 
 
 from models import Todo
@@ -22,6 +23,7 @@ def get_db():
         db.close()
 
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict,Depends(get_current_user)]
 
 # Pydantic model for request body validation
 class TodoRequest(BaseModel):
@@ -33,8 +35,8 @@ class TodoRequest(BaseModel):
 
 # get all todos
 @router.get("/", status_code=status.HTTP_200_OK)
-async def read_all(db: db_dependency):
-    return db.query(Todo).all()
+async def read_all(user:user_dependency,db: db_dependency):
+    return db.query(Todo).filter(Todo.owner_id==user.get('id')).all()
 
 # get todo by id using path parameter
 @router.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
@@ -46,8 +48,10 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
 
 # create todo using post method
 @router.post("/todo/", status_code=status.HTTP_201_CREATED)
-async def create_todo(db: db_dependency, todo_request: TodoRequest):
-    todo_model = Todo(**todo_request.dict())
+async def create_todo(user:user_dependency,db: db_dependency, todo_request: TodoRequest):
+    if user is None:
+        raise HTTPException(status_code=401,detail ="Authentications Failed")
+    todo_model = Todo(**todo_request.dict(),owner_id=user.get('id'))
 
     db.add(todo_model)
     db.commit()
