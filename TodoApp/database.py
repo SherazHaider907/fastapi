33efdc979146406f3197d8b422a2f833b6a3448c
SherazHaider907 <Sherazhaider665@gmail.com>
2026-04-20 10:58:32 +0500

    run first pytest as health check in main file

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db" # using sqlite database
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost:5432/TodoApplicationDatabase" # using postgresql database
