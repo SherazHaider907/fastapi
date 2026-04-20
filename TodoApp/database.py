434c38ef7978c30e75d753e6bdbcefef8a84990e
SherazHaider907 <Sherazhaider665@gmail.com>
2026-04-20 16:18:35 +0500

    add supabse

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -4,11 +4,12 @@ from sqlalchemy.orm import declarative_base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db" # using sqlite database
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost:5432/TodoApplicationDatabase" # using postgresql database
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.nzxanqpzuowpbxeyuxdh:75397561%21Qaz@aws-1-ap-northeast-1.pooler.supabase.com:6543/postgres"
 
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}) # only for sqlalchemy 
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}) # only for sqlalchemy 
 
-# engine = create_engine(SQLALCHEMY_DATABASE_URL) # using when database it postgresql
+engine = create_engine(SQLALCHEMY_DATABASE_URL) # using when database it postgresql
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
